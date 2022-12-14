20acd871216b5bbbee3196e51ac756e1263dd55e
Code Sandbox <hello@codesandbox.io>
2022-12-14 11:20:23 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.7062644a592c3c73a20107770c1e6dec.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0232ad4876deeed000e0b266f79aa085",
+    "url": "https://varunthaker.github.io/csb-s0igfv/index.html"
+  },
+  {
+    "revision": "4fd7a81c038202ac1436",
+    "url": "https://varunthaker.github.io/csb-s0igfv/static/js/2.824d600c.chunk.js"
+  },
+  {
+    "revision": "a1950ead3d70451b0996",
+    "url": "https://varunthaker.github.io/csb-s0igfv/static/js/main.c75a6f2a.chunk.js"
+  },
+  {
+    "revision": "ed8821e55251a2d0c055",
+    "url": "https://varunthaker.github.io/csb-s0igfv/static/js/runtime-main.26743e53.js"
+  }
+]);
